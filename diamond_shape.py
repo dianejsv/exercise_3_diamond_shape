2cf3d7560f64eb8c65c8af0c2623e00c323319bc
diane <villalino.136534100519@depedqc.ph>
2024-09-27 12:03:05 +0800

    Loop to print the upper half of the diamond

--- diamond_shape.py
+++ diamond_shape.py
@@ -5,3 +5,6 @@ n = int(input("Enter an odd integer for the diamond width: "))
 if n % 2 == 0:
     # Display an error message if the number is not odd
     print("\n\033[0;31mPlease provide an odd integer\033[0m")
+else:
+    # Print the upper part of the diamond
+    for i in range(n // 2 + 1):
